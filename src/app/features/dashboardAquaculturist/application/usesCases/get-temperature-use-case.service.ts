498989eaa4b0f7c214f87938ba9b65f391ebfe0d
JulioAlb453 <fulioibeto18@gmail.com>
2025-07-05 01:21:22 -0600

    implementation of hexagonal architecture in dashboard Aquaculturist

---
+++ src/app/features/dashboardAquaculturist/application/usesCases/get-temperature-use-case.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetTemperatureUseCaseService {
+  // Este servicio es un caso de uso que se encarga de obtener los datos de temperatura
+  constructor() { }
+}
+  
---
+++ src/app/features/dashboardAquaculturist/domain/model/time-range.enum.ts
@@ -0,0 +1,4 @@
+export interface TimeRangeEnum {
+    //Este modelo representa los rangos de tiempo disponibles para la visualización de datos
+    // "Diario", "Semanal", "Mensual
+}
