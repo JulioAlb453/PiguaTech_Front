41d168532c4874a0c57ceaf3c7fd0dc1a3e28d62
JulioAlb453 <fulioibeto18@gmail.com>
2025-07-07 22:44:04 -0600

    implementation hexagonal architecture to water turbidity in dashboard aquaculturist

---
+++ src/app/features/dashboardAquaculturist/waterMeasurement/WaterTurbidity/infraestructure/data-water-turbidity-reposotory.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+// este servicio es para obtener los datos de turbidez del agua
+// atraves de la API
+export class DataWaterTurbidityReposotoryService {
+
+  constructor() { }
+}
