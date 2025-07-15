bac2992aba2d5a667d511d4b1ffeae94c46114ec
JulioAlb453 <fulioibeto18@gmail.com>
2025-07-15 13:13:35 -0600

    add shelves overview father component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/views/login/login.component';
 import { ShelversOverviewComponent } from './shared/components/shelvers-overview/shelvers-overview.component';
+import { ShelversOverviewFatherComponentComponent } from './shared/components/shelvers-overview-father-component/shelvers-overview-father-component.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
   {
     path: 'home',
-    component: ShelversOverviewComponent,
+    component: ShelversOverviewFatherComponentComponent,
   },
   { path: 'login', component: LoginComponent },
 
--- src/app/shared/components/shelvers-overview/shelvers-overview.component.ts
+++ src/app/shared/components/shelvers-overview/shelvers-overview.component.ts
@@ -7,11 +7,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
-export type ShelftStatus = 'Optimo' | 'Atencion' | 'Alerta';
+export type ShelfStatus = 'Optimo' | 'Atencion' | 'Alerta';
 export interface Shelf {
   id: number;
   name: string;
-  status: ShelftStatus;
+  status: ShelfStatus;
   zone: string;
 }
 
@@ -28,24 +28,36 @@ export interface Shelf {
 export class ShelversOverviewComponent {
   //Se necesitara un componente padre para pasarle los datos
   @Input() title: string = 'Habitats activos';
-  @Input() shelves: Shelf[] = [];
-  @Input() basePath!: string;
+  @Input() shelves: Shelf[] = []; // Arreglo de los cards
+  @Input() basePath!: string; //Ruta para navegar entre los card respecto a su ID
   // Se notifica al componente padre que se creo un nuevo criadero
-  @Output() addShelf = new EventEmitter<void>();
+  @Output() addShelf = new EventEmitter<void>(); //Esto servira si es quiere aumentar el numero de estanques
 
   currentPage = 1;
-  itemsPerPage = 3;
+  itemsPerPage = 6;
 
+
+  //obtener el total de paginas que va a tener
   get totalPages(): number {
     return Math.ceil(this.shelves.length / this.itemsPerPage);
   }
 
+  //Obtener la alerta correspondiente
+  getOverallStatus(): string {
+    if(this.shelves.some (s => s.status  === 'Alerta')) return 'Alerta'
+    if(this.shelves.some(s => s.status === 'Atencion')) return 'Atencion'
+    return 'Optimo'
+  }
+
+  //para la paginacion
   get paginatedShelves(): Shelf[] {
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
     const endIndex = startIndex + this.itemsPerPage;
     return this.shelves.slice(startIndex, endIndex);
   }
 
+  // Esta funcion emite un evento para notificar al
+  // componente padre que se añadira un nuevo estanque
   onAddShelfClick(): void {
     this.addShelf.emit();
   }
