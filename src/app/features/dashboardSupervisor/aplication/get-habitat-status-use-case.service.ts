4370308fc1fc4a490f3199b5a7f1dbf2ff2023e0
JulioAlb453 <fulioibeto18@gmail.com>
2025-07-16 20:48:00 -0600

    feat(infra) add habitat status service

---
+++ src/app/features/dashboardSupervisor/aplication/get-habitat-status-use-case.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetHabitatStatusUseCaseService {
+
+  //caso de uso para obtener los datos de 
+  // los habitats
+
+  constructor() { }
+}
