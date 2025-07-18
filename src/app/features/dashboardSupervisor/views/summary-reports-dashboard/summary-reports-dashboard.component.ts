538061d8a31863516d6e791ba245048bcf2e7687
RichieCast07 <richardaton234@gmail.com>
2025-07-18 11:52:45 -0600

    add scss the html

---
+++ src/app/features/dashboardSupervisor/views/summary-reports-dashboard/summary-reports-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-summary-reports-dashboard',
+  standalone: false,
+  templateUrl: './summary-reports-dashboard.component.html',
+  styleUrl: './summary-reports-dashboard.component.scss'
+})
+export class SummaryReportsDashboardComponent {
+
+}
